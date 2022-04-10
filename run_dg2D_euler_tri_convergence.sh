83dd4a02c08b64d969ed64f0f04f3b24ff9e6639
Yimin Lin <yiminlin@pop-os.localdomain>
2022-04-10 02:18:04 -0500

    Add script

---
+++ run_dg2D_euler_tri_convergence.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for N in 1 2 3 4
+do
+for K1D in 2 4 8 16 32
+do
+  nohup julia -t 4 examples/IDP/dg2D_euler_tri_convergence.jl $N $K1D >> dg2D_euler_tri_convergence_hist.log 2>&1 &
+  wait $!
+done
+done
